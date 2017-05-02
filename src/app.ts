ac0f228c26a1f4ea43600ab15cb0ff35f0eeee72
Eric Helin <erichelin@quickenloans.com>
2017-05-01 22:24:33 -0400

    added hapi

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('inside app.ts');
+import Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+server.route({
+    method: 'GET',
+    path:'/',
+    handler: function (request: any, reply: any) {
+        return reply('Welcome to the API');
+    }
+});
+
+server.start((err: any) => {
+    if (err) { throw err; }
+    console.log('Server running at:', server.info.uri);
+});
