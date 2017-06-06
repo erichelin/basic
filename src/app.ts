ecf63353665c4b089eb8bc975f7ca359728737b8
Eric Helin and Tommy Ru pair+ehelin+tru@quickenloans.com <pair+ehelin+tru@quickenloans.com>
2017-06-06 15:54:13 -0400

    added dynamo db client

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,37 @@ const port = process.env.PORT || 8080;
 
 server.connection({port});
 
+// main ==========================================
 server.route({
     method: 'GET',
     path:'/',
     handler: function (request: any, reply: any) {
         return reply('Welcome to the API');
+    }
+});
+
+// dynamo db ======================================
+server.route({
+    method: 'POST',
+    path:'/dynamoDbCreate',
+    handler: function (request: any, reply: any) {
+        //return reply('Welcome to the API');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path:'/dynamoDbRead',
+    handler: function (request: any, reply: any) {
+        //return reply('Welcome to the API');
+    }
+});
+
+server.route({
+    method: 'POST',
+    path:'/dynamoDbDelete',
+    handler: function (request: any, reply: any) {
+        //return reply('Welcome to the API');
     }
 });
 
---
+++ src/dynamoDb/__test__/dynamoDb.test.ts
@@ -0,0 +1,26 @@
+import {DatabaseClient} from '../dynamoDb';
+import {expect} from 'chai';
+
+describe('dynamoDb', () => {
+    it('save, retrieve and delete record', async () => {
+        const dbClient = new DatabaseClient();
+
+        //save/get ------------------------
+        await dbClient.save();
+        const result = await dbClient.get();
+
+        expect(result).to.not.be.undefined;
+
+        console.log('result');
+        console.log(result);
+
+        //delete --------------------------
+        await dbClient.delete();
+        const afterDeleteResult = await dbClient.get();
+
+        expect(afterDeleteResult).to.eql({});
+
+        console.log('afterDeleteResult');
+        console.log(afterDeleteResult);
+    });
+});
